4511455884e21124f2d32e02d4dbbfbea8eb3ab2
Anthony Kim <fbssaejun@gmail.com>
2021-09-13 22:36:11 +0000

    log error message if any

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store = gets.chomp
+@create_store = Store.create(name: store)
+
+if @create_store.errors.any? # If there are errors, do something
+  @create_store.errors.each do |attribute, message|
+    puts attribute, message
+  end
+end
