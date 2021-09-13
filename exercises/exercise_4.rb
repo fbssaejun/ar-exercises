ed688bd51e1bc51cf80c4ebeb24ca59f87d0f2cf
Anthony Kim <fbssaejun@gmail.com>
2021-09-13 21:03:26 +0000

    find and load subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Burnaby", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Richmond", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Gastown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "Mens apparel"
+@mens_stores.each do |store|
+  puts "Name : #{store.name}, Annual revenue : #{store.annual_revenue}."
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue <= 1000000")
+
+puts "Womens apparel"
+@womens_stores.each do |store|
+  puts "Name : #{store.name}, Annual revenue : #{store.annual_revenue}."
+end
