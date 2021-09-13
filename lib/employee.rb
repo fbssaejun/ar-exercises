f6524c50376a0bc92ee7c4facd569eb157f98b42
Anthony Kim <fbssaejun@gmail.com>
2021-09-13 21:32:05 +0000

    add validations for employee clasS

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, present: true
+  validates :last_name, present: true
+  validates :hourly_rate, inclusion: { in: 40..200}
+  validates :store, present: true
 end
