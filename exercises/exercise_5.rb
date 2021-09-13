4366b57ee181755334061241cef68d7404ec84a2
Anthony Kim <fbssaejun@gmail.com>
2021-09-13 21:11:10 +0000

    fetch revenue data from each store's data

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#Total revenue in the company
+puts Store.sum(:annual_revenue)
+
+#Average revenue in the company
+puts Store.average(:annual_revenue)
+
+#Number of stores making more than 1M in revenue
+puts Store.where("annual_revenue >= 1000000").count
